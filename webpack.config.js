2f64410693f5c4d660cba9c5b3fd6cf9bcea1188
Haderson Bullón <hrbullon@gmail.com>
2020-02-09 23:38:53 -0400

    Instalaci[on de dependencias

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+       title: 'Webpack basic project',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+    chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader',
+            ],
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+            'file-loader',
+            ],
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+            'file-loader',
+            ],
+        },
+    ],
+  },
+};
