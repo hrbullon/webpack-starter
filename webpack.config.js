463f86ff659ea326583b1b01c24d5ceefea82a90
Haderson Bullon <hrbullon@gmail.com>
2020-02-10 10:30:12 -0400

    Hashng, ChunksFiles, Vendor, Favicon

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,59 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-       title: 'Webpack basic project',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    splitChunks: {
-    chunks: 'all',
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
     },
-  },
-  module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-            'style-loader',
-            'css-loader',
-            ],
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-            'file-loader',
-            ],
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-            'file-loader',
-            ],
-        },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Webpack Basic Project',
+            favicon: './src/favicon.ico'
+        })
     ],
-  },
+    output: {
+        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        moduleIds: 'hashed',
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
 };
